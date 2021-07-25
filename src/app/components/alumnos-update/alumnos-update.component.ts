1f26566edb8f2313a4d910558c554b6a224e5f02
547343578 <wanglucas5@gmail.com>
2021-07-25 21:50:32 +0200

    update

--- src/app/components/alumnos-update/alumnos-update.component.ts
+++ src/app/components/alumnos-update/alumnos-update.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {Alumno} from '../../clases/Alumno';
 import {AlumnoServiceService} from '../../services/alumno-service.service';
-import {Router} from '@angular/router';
+import {ActivatedRoute, Router} from '@angular/router';
 import swal from 'sweetalert2';
 
 
@@ -11,20 +11,29 @@ import swal from 'sweetalert2';
   styleUrls: ['./alumnos-update.component.css']
 })
 export class AlumnosUpdateComponent implements OnInit {
-
+  
   alumno:Alumno = new Alumno();
 
-  constructor(private alumnoService:AlumnoServiceService, private router:Router) { }
+  constructor(private alumnoService:AlumnoServiceService, private router:ActivatedRoute, private router_:Router) { }
 
   ngOnInit(): void {
+    const params = this.router.snapshot.params;
+    if(params.id){
+      this.alumnoService.getAlumno(params.id)
+      .subscribe(
+        res => {
+          this.alumno = res;
+        }
+      )
+    }
   }
 
   modificarAlumno(){
-    return this.alumnoService.updateAlumno(this.alumno)
-    .subscribe(
-      res =>{this.router.navigate(['/']),
-      swal.fire('Alumno modificado', `${res.nombre} ${res.apellidos} modificado`, 'success' ) 
-    }
+    return this.alumnoService.updateAlumno(this.alumno).subscribe(
+      res => {
+        this.router_.navigate(['/']),
+        swal.fire('Alumno modificado', `${this.alumno.nombre} ${this.alumno.apellidos} modificado`, 'success'  )
+      }
     )
   }
 }
